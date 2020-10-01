d468ce3790b24d993761f28834895bede7e4ccf6
BenYoung4 <ben.young999@hotmail.co.uk>
2020-10-01 13:44:45 +0100

    Fizz buzz kata - add FizzBuzz and FooBar

--- FizzBuzz/src/test/java/FizzBuzzTest.java
+++ FizzBuzz/src/test/java/FizzBuzzTest.java
@@ -1,5 +1,53 @@
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
 
 class FizzBuzzTest {
+
+    @Test
+    public void ShouldReturnNumberOne() {
+        assertEquals("1", new FizzBuzz().number(1));
+    }
+
+    @Test
+    public void ShouldReturnNumberTwo (){
+        assertEquals("2", new FizzBuzz().number(2));
+    }
+
+    @Test
+    public void ShouldReturnFizz() {
+        assertEquals("Fizz",new FizzBuzz().number(3));
+    }
+
+    @Test
+    public void ShouldReturnFour() {
+        assertEquals("4",new FizzBuzz().number(4));
+    }
+
+    @Test
+    public void ShouldReturnBuzz() {
+        assertEquals("Buzz",new FizzBuzz().number(5));
+    }
+
+    @Test
+    public void ShouldReturnFoo() {
+        assertEquals("Foo",new FizzBuzz().number(7));
+    }
+
+    @Test
+    public void ShouldReturnBar() {
+        assertEquals("Bar",new FizzBuzz().number(11));
+    }
+
+    @Test
+    public void ShouldReturnFizzBuzz() {
+        assertEquals("FizzBuzz",new FizzBuzz().number(15));
+    }
+
+    @Test
+    public void ShouldReturnFooBar() {
+        assertEquals("FooBar",new FizzBuzz().number(77));
+    }
 
 }
